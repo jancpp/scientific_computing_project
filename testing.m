32cbbd754a11e9eaf28b75dc911c5b8b226fb025
Jan Polzer <janman74@gmail.com>
2018-11-27 14:48:39 -0600

    Fix Vandermonde

--- testing.m
+++ testing.m
@@ -14,9 +14,6 @@ for x1=1:5
 end
 
 % Vandermonde polynomial function
-x = vandermonde(dp1);
-vandermonde_polynomial1 = @(t) x(1) + x(2)*t + x(3)*(t^2) + x(4)*(t^3) + x(5)*(t^4);
-
 subplot(3,3,1);
 hold on;
 title('Vandermonde #1');
@@ -26,12 +23,7 @@ for i=1:5
     plot(xdp1,ydp1,'ro'); % data points
 end
 
-xx = linspace(0.6,1,10);
-for i=1:10
-    yy(i) = vandermonde_polynomial1(xx(i));
-end
-plot(xx,yy,'-g'); % function
-
+fplot(vandermonde(dp1), [0.6 1]) % function
 
 % Newton polynomial function
 subplot(3,3,2);
@@ -43,7 +35,7 @@ for i=1:5
     plot(xdp1,ydp1,'ro'); % data points
 end
 newton_polynomial1 = newton_polynomial(dp1(:,1), dp1(:,2)); 
-fplot(newton_polynomial1, [0.6 1])
+fplot(newton_polynomial1, [0.6 1]) % function
 
 % Lagrange polynomial function
 subplot(3,3,3);
@@ -55,7 +47,7 @@ for i=1:5
     plot(xdp1,ydp1,'ro'); % data points
 end
 lagrange_polynomial1 = lagrange(dp1(:,1), dp1(:,2)); 
-fplot(lagrange_polynomial1, [0.6 1])
+fplot(lagrange_polynomial1, [0.6 1]) % function
 
 % =============================
 % data point set #2 f(x)=1/(1 + (12 * x^2)), on [-1,1], 15 equally-spaced points
@@ -70,9 +62,6 @@ for x2=1:15
 end
 
 % Vandermonde polynomial function
-x = vandermonde(dp2);
-vandermonde_polynomial2 = @(t) x(1) + x(2)*t + x(3)*(t^2) + x(4)*(t^3) + x(5)*(t^4) + x(6)*(t^5) + x(7)*(t^6) + x(8)*(t^7)+ x(9)*(t^8) + x(10)*(t^9) + x(11)*(t^10) + x(12)*(t^11) + x(13)*(t^12) + x(14)*(t^13) + x(15)*(t^14);
-
 subplot(3,3,4);
 hold on;
 title('Vandermonde #2');
@@ -81,12 +70,8 @@ for i=1:15
     ydp2 = dp2(i,2);
     plot(xdp2,ydp2,'ro'); % data points
 end
-
-xx = linspace(-1,1,15);
-for i=1:15
-    yy(i) = vandermonde_polynomial1(xx(i));
-end
-plot(xx,yy,'-g'); % function
+ 
+fplot(vandermonde(dp2), [-1 1]) % function
 
 % Newton polynomial function
 subplot(3,3,5);
@@ -98,7 +83,7 @@ for i=1:15
     plot(xdp2,ydp2,'ro'); % data points
 end
 newton_polynomial2 = newton_polynomial(dp2(:,1), dp2(:,2)); 
-fplot(newton_polynomial2, [-1 1])
+fplot(newton_polynomial2, [-1 1]) % function
 
 % Lagrange polynomial function
 subplot(3,3,6);
@@ -110,7 +95,7 @@ for i=1:15
     plot(xdp2,ydp2,'ro'); % data points
 end
 lagrange_polynomial2 = lagrange(dp2(:,1), dp2(:,2)); 
-fplot(lagrange_polynomial2, [-1 1])
+fplot(lagrange_polynomial2, [-1 1]) % function
 
 % =============================
 % data point set #3
@@ -120,9 +105,6 @@ dp3 = [ 1994 67.052; 1995 68.008; 1996 69.803; 1997 72.024; 1998 73.400; 1999 72
 
 
 % Vandermonde polynomial function
-x3 = vandermonde(dp3); % Fix  Error  Error  Error  Error  Error  Error  Error  Error  Error  Error  Error  Error
-vandermonde_polynomial3 = @(t) x3(1) + x3(2)*t + x3(3)*(t^2) + x3(4)*(t^3) + x3(5)*(t^4) + x3(6)*(t^5) + x3(7)*(t^6) + x3(8)*(t^7) + x3(9)*(t^8) + x3(10)*(t^9) ;
-
 subplot(3,3,7);
 hold on;
 title('Vandermonde #3');
@@ -132,11 +114,7 @@ for i=1:1:10
     plot(xdp3,ydp3,'ro'); % data points
 end
 
-xx = linspace(1994,2003,100);
-for i=1:100
-    yy(i) = vandermonde_polynomial3(xx(i));
-end
-plot(xx,yy,'-g'); % function
+fplot(vandermonde(dp3), [1994 2003]) % function
 
 
 % Newton polynomial function
@@ -149,7 +127,7 @@ for i=1:1:10
     plot(xdp3,ydp3,'ro'); % data points
 end
 newton_polynomial3 = newton_polynomial(dp3(:,1), dp3(:,2)); 
-fplot(newton_polynomial3, [1994 2003])
+fplot(newton_polynomial3, [1994 2003]) % function
 
 % Lagrange polynomial function
 subplot(3,3,9);
@@ -162,4 +140,4 @@ for i=1:1:10
 end
 
 lagrange_polynomial3 = lagrange(dp3(:,1), dp3(:,2)); 
-fplot(lagrange_polynomial3, [1994 2003])
+fplot(lagrange_polynomial3, [1994 2003]) % function
--- vandermonde.m
+++ vandermonde.m
@@ -1,13 +1,14 @@
-function [x] = vandermonde(dp)
+function [full] = vandermonde(dp)
 % interpol_vandermonde is a Vandermonde interpolation function 
 % that works for any number of input data points dp
 % dp  - input data points [ (t1,y1), (t2,y2), (t3,y3), ... (tn,yn) ]
 % x   - coeficients that plugs in the polynomial equation 
 % p(t) = x(1) + x(2)*t +...+ x(n)*t^(n-1)
 
-    [numOfPoints,~] = size(dp); % number of data inputs
+    numOfPoints = length(dp); % number of data inputs
     A = zeros(numOfPoints);
     y = zeros(numOfPoints,1);
+    syms t;
     for row=1:numOfPoints
         A(row, 1) = 1;
         for col=2:numOfPoints
@@ -17,6 +18,10 @@ function [x] = vandermonde(dp)
     end
     
     x = GEPP(A, y); 
+    full = 0;
+    for i=1:numOfPoints
+        full = full + x(i)*t^(i-1);
+    end
     
 end
 
